e5302fdc52ec00dd3317d4b5fcef3cf2108a2643
zyasuda <yasuda.keigo@gmail.com>
2024-05-20 18:59:23 +0900

    速度を倍にしました

--- pixel04.py
+++ pixel04.py
@@ -6,13 +6,13 @@ i = 0
 def update():
   global dir,i
   if dir == 1:
-    i -= 1
-    if i == 0:
+    i -= 2  # 移動速度を倍にする
+    if i <= 0:
       dir = 0
 
   if dir == 0:
-      i += 1
-      if i == 120:
+      i += 2  # 移動速度を倍にする
+      if i >= 120:
         dir = 1
 
 def draw():
